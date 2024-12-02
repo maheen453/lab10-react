cfe5565278b545854958b514b7d132aedc122267
maheen <qayyummaheen@gmail.com>
2024-12-02 04:21:13 -0500

    app.css

--- src/App.js
+++ src/App.js
@@ -16,13 +16,17 @@ function App() {
         <p>I navigated the directories that were made during the create-react-app command to find App.js and modified it to include this text using simple javascript. Then I added styling in App.css.</p>
         <p>I connected to GitHub by using the repository automatically initialized by React during the create-react-app command and deployed the project directly from that GitHub repository using Netlify.</p>
         <h2>Challenges and Solutions</h2>
-        <ul>
-          <p><strong>Challenge:</strong> Knowing what commands to use and how to navigate the React project directories.</p>
-          <p><strong>Solution:</strong> Referenced React documentation and tutorials.</p>
-          <p><strong>Challenge:</strong>I didn’t know how to host the React project after building it.</p>
-          <p><strong>Solution:</strong>I researched hosting services, found Netlify, created an account, and followed its simple steps to deploy directly from GitHub.</p>
-          
-        </ul>
+        <p><strong>Challenge:</strong> Knowing what commands to use and how to navigate the React project directories.
+        <br />
+        <strong>Solution:</strong> Referenced React documentation and tutorials.</p>
+        <p><strong>Challenge:</strong> I didn’t know how to host the React project after building it.
+        <br />
+        <strong>Solution:</strong> I researched hosting services, found Netlify, created an account, 
+        and followed its simple steps to deploy directly from GitHub.</p>
+        <p><strong>Challenge:</strong> After making a change to the code, the project wouldn’t deploy successfully.
+        <br />
+        <strong>Solution:</strong> I checked the Netlify deployment logs, identified a syntax error in my code, 
+        and went back to fix it before re-deploying successfully.</p>
       </header>
     </div>
   );
